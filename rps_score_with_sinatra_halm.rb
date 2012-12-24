7f0d364f3f67edbc0adc18b415941190c62cb1bc
yeray <yerayrm90@gmail.com>
2012-12-24 14:28:32 +0000

    Score finalizado

--- rps_score_with_sinatra_halm.rb
+++ rps_score_with_sinatra_halm.rb
@@ -1,20 +1,34 @@
 require 'sinatra'
 require 'haml'
 
-# before we process a route we'll set the response as plain text
-# and set up an array of viable moves that a player (and the
-# computer) can perform.
+#use Rack::Session::Pool, :expire_after => 2592000
+set :session_secret, ENV["SESSION_KEY"] || 'too secret'
+
+configure do
+  enable :sessions
+end
+
 before do
   @defeat = { rock: :scissors, paper: :rock, scissors: :paper}
   @throws = @defeat.keys
+  @score = {win: 0, tie: 0, lose: 0}
 end
 
 get '/' do
-    haml :index
+  if session[:win] == nil 
+    session[:win] = 0
+    session[:tie] = 0
+    session[:lose] = 0  
+  end
+  haml :index
+end
+
+get '/clear' do
+  session.clear
+  redirect '/'
 end
 
 get '/throw/' do
-  # the params hash stores querystring and form data
 	@option = params['option'.to_sym].downcase
 	@player_throw = @option.to_sym
 
@@ -24,13 +38,16 @@ get '/throw/' do
 
   if @player_throw == @computer_throw 
     @answer = "Se ha producido un empate."
-    #erb :index
+    @score[:tie] += 1
+    session[:tie] += 1
   elsif @player_throw == @defeat[@computer_throw]
     @answer = "El ordenador gana; #{@computer_throw} derrota a #{@player_throw}."
-    #erb :index
+    @score[:lose] += 1
+    session[:lose] += 1
   else
     @answer = "Muy bien. #{@player_throw} vence a #{@computer_throw}."
-    #erb :index
+    @score[:win] += 1
+    session[:win] += 1
   end
   haml :resultado
 end
